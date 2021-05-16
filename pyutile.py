44fa80d7e043192e5b1ab2603f152ce450a5a99f
liji <499544968@qq.com>
2021-05-16 19:45:13 +0800

    plt

--- pyutile.py
+++ pyutile.py
@@ -4,9 +4,12 @@ import matplotlib.pyplot as plt
 import os
 import sys
 
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
+
 pyutile = sys.modules[__name__]
 
 def init():
     plt.rcParams['font.sans-serif'] = ['SimHei'] # 步骤一（替换sans-serif字体）
     plt.rcParams['axes.unicode_minus'] = False   # 步骤二（解决坐标轴负数的负号显示问题）
+    os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
     print("done")
